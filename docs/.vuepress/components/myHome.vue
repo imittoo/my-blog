fb5ae8fc569dd7c10ce73f76d6fea1bb460dee0b
Meidongwei <450650472@qq.com>
2018-08-31 18:03:57 +0800

    update

--- docs/.vuepress/components/myHome.vue
+++ docs/.vuepress/components/myHome.vue
@@ -3,6 +3,18 @@
     <img class="heroImg" :src="data.heroImage" alt="hero">
     <h1 class="title">{{ data.title }}</h1>
     <!-- <a :href="data.actionLink">{{ data.actionText }}</a> -->
+    <ul class="bg-bubbles">
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
   </div>
 </template>
 <script>
@@ -15,13 +27,121 @@ export default {
 }
 </script>
 <style lang="stylus" scoped>
+* {
+  padding 0
+  margin 0
+}
 .hero
-  height 500px
+  height calc(100vh - 58px)
   display flex
   flex-direction column
   justify-content center
   align-items center
+  background linear-gradient(to bottom right, #50A3A2, #53E3A6)
   .heroImg
     width 200px
+    border-radius 50px
+
+.bg-bubbles {
+  position: absolute;
+  /* 使气泡背景充满整个屏幕 */
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  /* 如果元素内容超出给定的宽度和高度，
+   * overflow 属性可以隐藏滚动条
+   */
+  overflow: hidden;
+}
+.bg-bubbles li {
+  position: absolute;
+  /* bottom 的设置是为了营造出气泡从页面底部冒出的效果 */
+  bottom: -160px;
+  /* 默认的气泡大小 */
+  width: 40px;
+  height: 40px;
+  background-color: rgba(255, 255, 255, 0.15);
+  list-style: none;
+  /* 使用自定义动画使气泡渐现、上升和翻滚 */
+  animation: square 15s infinite;
+  transition-timing-function: linear;
+}
+.bg-bubbles li:nth-child(1) {
+  left: 10%;
+}
+.bg-bubbles li:nth-child(2) {
+  left: 20%;
+  width: 90px;
+  height: 90px;
+  animation-delay: 2s;
+  animation-duration: 7s;
+}
+.bg-bubbles li:nth-child(3) {
+  left: 25%;
+  animation-delay: 4s;
+}
+.bg-bubbles li:nth-child(4) {
+  left: 40%;
+  width: 60px;
+  height: 60px;
+  animation-duration: 8s;
+  background-color: rgba(255, 255, 255, 0.3);
+}
+.bg-bubbles li:nth-child(5) {
+  left: 70%;
+}
+.bg-bubbles li:nth-child(6) {
+  left: 80%;
+  width: 120px;
+  height: 120px;
+  animation-delay: 3s;
+  background-color: rgba(255, 255, 255, 0.2);
+}
+.bg-bubbles li:nth-child(7) {
+  left: 32%;
+  width: 160px;
+  height: 160px;
+  animation-delay: 2s;
+}
+.bg-bubbles li:nth-child(8) {
+  left: 55%;
+  width: 20px;
+  height: 20px;
+  animation-delay: 4s;
+  animation-duration: 15s;
+}
+.bg-bubbles li:nth-child(9) {
+  left: 25%;
+  width: 10px;
+  height: 10px;
+  animation-delay: 2s;
+  animation-duration: 12s;
+  background-color: rgba(255, 255, 255, 0.3);
+}
+.bg-bubbles li:nth-child(10) {
+  left: 85%;
+  width: 160px;
+  height: 160px;
+  animation-delay: 5s;
+}
+@keyframes square {
+  0% {
+    opacity: 0.5;
+    transform: translateY(0px) rotate(45deg);
+  }
+  25% {
+    opacity: 0.75;
+    transform: translateY(-400px) rotate(90deg)
+  }
+  50% {
+    opacity: 1;
+    transform: translateY(-600px) rotate(135deg);
+  }
+  100% {
+    opacity: 0;
+    transform: translateY(-1000px) rotate(180deg);
+  }
+}
 </style>
 
